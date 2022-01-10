70f7f8efe2bff36b51a79ea07fb04d5e96f44087
p1eXu5 <v1adp1exu5@gmail.com>
2022-01-10 13:22:05 +0300

    [ chore ] - add ParentWidthConverter

---
+++ src/FableTranspiler.WpfClient/Converters/ParentWidthConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FableTranspiler.WpfClient.Converters
+{
+    public class ParentWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (
+                values[0] is double width
+                && values[1] is bool showTsContent
+                && values[2] is bool showFelizContent
+                && values[3] is bool showFableContent
+                && values[4] is Thickness margin) 
+            { 
+                var factor =
+                    (showTsContent ? 1 : 0)
+                    + (showFelizContent ? 1 : 0)
+                    + (showFableContent ? 1 : 0);
+
+                object rv =
+                    width / (factor == 0 ? 1 : factor) - (margin.Left + margin.Right);
+
+                return rv;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[] { 0, false, false, false, new Thickness() };
+        }
+    }
+}
