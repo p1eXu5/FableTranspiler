cf472eac038921643cd26b58008765be5a367341
p1eXu5 <v1adp1exu5@gmail.com>
2022-01-16 18:52:07 +0300

    add test cases

--- test/FableTranspiler.Tests/VmAdapters/FsInterpreter/FsInterpreter.FacadeTests.fs
+++ test/FableTranspiler.Tests/VmAdapters/FsInterpreter/FsInterpreter.FacadeTests.fs
@@ -5,13 +5,15 @@ open FsUnit
 open FableTranspiler.Parsers
 open FableTranspiler.VmAdapters
 open FableTranspiler.VmAdapters.FsInterpreter
+open FableTranspiler.Tests.VmAdapters.TestCaseSources.InterfaceTestCaseSources
 
 module FsDocumentInterpreterTests =
 
     let store : FsStatementStore = FableTranspiler.Infrastruture.FsStatementInMemoryStore.store
 
+
     [<Test>]
-    let ``interpret exported interface statement with union option field type to Fable test`` () =
+    let ``interpret exported interface statement with union option field type to Fable with expected Name test`` () =
         let input = """
             export interface Foo {
                 smooth?: boolean | string | undefined;
@@ -32,7 +34,22 @@ module FsDocumentInterpreterTests =
         let vm = Facade.interpret None "test" store fableInterpreters statement
         vm[1].FsStatement.Name() |> should equal "Foo" // [0] - is "module ..."
 
-        let expectedField = "abstract smooth : U2<bool, string> option"
+
+    [<TestCaseSource(typeof<TestCases>, nameof TestCases.ExportCases)>]
+    let ``interpret exported interface statement with union option field type to Fable test`` (input: string, expectedField: string) =
+        let statement = 
+            match Parser.document input with
+            | Ok s -> s
+            | Error err -> AssertionException(err) |> raise
+
+
+        let fableInterpreters =
+            {
+                InterpretPlainFableInterface = Fable.interpretPlainFableInterface
+            }
+
+        let vm = Facade.interpret None "test" store fableInterpreters statement
+
         vm[1].FsStatement.StringContent() |> should haveSubstring expectedField
 
 
---
+++ test/FableTranspiler.Tests/VmAdapters/FsInterpreter/TestCaseSources/InterfaceTestCaseSources.fs
@@ -0,0 +1,36 @@
+﻿module FableTranspiler.Tests.VmAdapters.TestCaseSources.InterfaceTestCaseSources
+
+open System.Collections
+open NUnit.Framework
+
+let private exportedInterfaceFormatted =
+    """
+        export interface Foo {
+            {field}
+        }
+    """
+
+let exportedInterface field =
+    exportedInterfaceFormatted.Replace("{field}", field)
+
+
+type TestCases () =
+
+    static member ExportCases : IEnumerable =
+        seq {
+
+            TestCaseData(
+                exportedInterface "to: string;" |> box,
+                "abstract to : string"
+            ).SetName("Field: 'to: string;'")
+
+            TestCaseData(
+                exportedInterface "smooth?: boolean | string | undefined;" |> box,
+                "abstract smooth : U2<bool, string> option"
+            ).SetName("Field: 'smooth?: boolean | string | undefined;'")
+
+            TestCaseData(
+                exportedInterface "onClick?(): void;" |> box,
+                "abstract onClick : (unit -> unit) option"
+            ).SetName("Field: 'onClick?(): void;'")
+        }
